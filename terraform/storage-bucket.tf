2eb1edf04f0585201ccce7057dd69e24eb182f84
WasaPupkin <wasamyname@gmail.com>
2022-02-05 21:42:41 +0300

    try fix test

--- terraform/storage-bucket.tf
+++ terraform/storage-bucket.tf
@@ -1,11 +1,3 @@
-terraform {
-  required_providers {
-    yandex = {
-      source  = "yandex-cloud/yandex"
-      version = "0.35"
-    }
-  }
-}
 provider "yandex" {
   service_account_key_file = var.service_account_key_file
   cloud_id                 = var.cloud_id
